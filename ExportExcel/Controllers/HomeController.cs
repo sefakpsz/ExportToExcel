2821f37961bf36bb753bd456b6b456470ece6c2b
PC <PC@DESKTOP-HLG81VC>
2022-08-08 11:04:12 +0300

    Add project files.

---
+++ ExportExcel/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using ClosedXML.Excel;
+using ExportExcel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExportExcel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private List<User> users = new()
+        {
+            new User{Id=1,Name="a"},
+            new User{Id=2,Name="b"},
+            new User{Id=3,Name="c"},
+            new User{Id=4,Name="d"}
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExportToExcel()
+        {
+            using var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Users");
+            var currentRow = 1;
+            worksheet.Cell(currentRow, 1).Value = "Id";
+            worksheet.Cell(currentRow, 2).Value = "Username";
+            foreach (var user in users)
+            {
+                currentRow++;
+                worksheet.Cell(currentRow, 1).Value = user.Id;
+                worksheet.Cell(currentRow, 2).Value = user.Name;
+            }
+            using var stream = new MemoryStream();
+            workbook.SaveAs(stream);
+            var content = stream.ToArray();
+            return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "users.xlsx");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
